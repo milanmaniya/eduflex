bd4b0b20ad78897c0b431845ea4bb60e67520d91
milanmaniya <milanmaniya2035@gmail.com>
2024-01-12 22:16:26 +0530

    done

---
+++ lib/screen/teacher/dashboard/navigation_menu_screen/home_screen/widget/all_subject_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AllSubjectScreen extends StatefulWidget {
+  const AllSubjectScreen({super.key});
+
+  @override
+  State<AllSubjectScreen> createState() => _AllSubjectScreenState();
+}
+
+class _AllSubjectScreenState extends State<AllSubjectScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(),
+    );
+  }
+}
