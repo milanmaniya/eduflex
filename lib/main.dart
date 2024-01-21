dc62754d7dab674d5d145b47af91d4a53e9caba5
milanmaniya <milanmaniya2035@gmail.com>
2024-01-21 11:18:00 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:zego_zimkit/zego_zimkit.dart';
 
 void main() async {
   // widget binding
@@ -12,6 +13,17 @@ void main() async {
 
   // getx local storage
   await GetStorage.init();
+
+  int appId = 646832829;
+  String appSignId =
+      '37069f0f58db671091a3a0e7dcd51250986c5e4c5507b9237c96f131c9c82192';
+
+  ZIMAppConfig appConfig = ZIMAppConfig();
+
+  appConfig.appID = appId;
+  appConfig.appSign = appSignId;
+
+  ZIM.create(appConfig);
 
   //initialize firebase & authentication repository
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
