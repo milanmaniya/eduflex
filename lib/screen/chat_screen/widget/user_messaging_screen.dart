435eb78384ff79510e6437cf2c698456f0b25df7
milanmaniya <milanmaniya2035@gmail.com>
2024-01-24 00:18:38 +0530

    done

--- lib/screen/chat_screen/widget/user_messaging_screen.dart
+++ lib/screen/chat_screen/widget/user_messaging_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:logger/logger.dart';
@@ -12,6 +13,8 @@ class UserMessagingScreen extends StatefulWidget {
 }
 
 class _UserMessagingScreenState extends State<UserMessagingScreen> {
+  get child => null;
+
   @override
   void initState() {
     Logger().i(widget.data.toString());
@@ -22,23 +25,63 @@ class _UserMessagingScreenState extends State<UserMessagingScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 4,
         automaticallyImplyLeading: false,
-        flexibleSpace: _appBar(),
-      ),
-    );
-  }
-
-  Widget _appBar() {
-    return Row(
-      children: [
-        IconButton(
-          onPressed: () {},
-          icon: const Icon(
-            Iconsax.activity,
-            color: Colors.black54,
+        toolbarHeight: 64,
+        flexibleSpace: Padding(
+          padding: const EdgeInsets.only(
+            top: 40,
+          ),
+          child: InkWell(
+            onTap: () {},
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                IconButton(
+                  onPressed: () => Navigator.pop(context),
+                  icon: const Icon(Iconsax.arrow_left),
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(100),
+                  child: CachedNetworkImage(
+                    height: 45,
+                    width: 45,
+                    fit: BoxFit.cover,
+                    imageUrl: widget.data['image'],
+                    errorWidget: (context, url, error) => const CircleAvatar(
+                      child: Icon(Iconsax.people),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 15,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      widget.data['userName'],
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.black87.withOpacity(0.7),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const Text(
+                      'Last seen not available',
+                      style: TextStyle(
+                        fontSize: 12.5,
+                        color: Colors.black87,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
-      ],
+      ),
     );
   }
 }
