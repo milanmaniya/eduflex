a49b42b470f7fe1fce0fe505f3264c61d3a790f6
milanmaniya <milanmaniya2035@gmail.com>
2024-01-25 00:50:39 +0530

    done

--- lib/screen/chat_screen/widget/chat_user_card.dart
+++ lib/screen/chat_screen/widget/chat_user_card.dart
@@ -1,8 +1,11 @@
 // ignore: unused_import
 import 'dart:ffi';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:eduflex/screen/chat_screen/apis/apis.dart';
+import 'package:eduflex/screen/chat_screen/model/chat_user_model.dart';
 import 'package:eduflex/screen/chat_screen/widget/user_messaging_screen.dart';
 import 'package:eduflex/utils/helper/helper_function.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
@@ -20,49 +23,78 @@ class ChatUserCard extends StatefulWidget {
 }
 
 class _ChatUserCardState extends State<ChatUserCard> {
+  Message? msg;
+
   @override
   Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: InkWell(
-        onTap: () => Get.to(
-          () => UserMessagingScreen(
-            data: widget.data,
+    return StreamBuilder(
+      stream: APIS.getLastMessage(widget.data['id']),
+      builder: (context, snapshot) {
+        final data = snapshot.data!.docs;
+
+        final list = data.map((e) => Message.fromJson(e.data())).toList();
+
+        if (list.isNotEmpty) {
+          msg = list[0];
+        }
+
+        return Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
           ),
-        ),
-        child: Container(
-          alignment: Alignment.center,
-          height: 90,
-          child: ListTile(
-            leading: ClipRRect(
-              borderRadius:
-                  BorderRadius.circular(THelperFunction.screenHeight() * .3),
-              child: CachedNetworkImage(
-                height: 50,
-                width: 50,
-                imageUrl: widget.data['image'],
-                errorWidget: (context, url, error) =>
-                    const CircleAvatar(child: Icon(Iconsax.people)),
+          child: InkWell(
+            onTap: () => Get.to(
+              () => UserMessagingScreen(
+                data: widget.data,
               ),
             ),
-            title: Text(widget.data['userName']),
-            trailing: Container(
-              height: 12,
-              width: 12,
-              decoration: const BoxDecoration(
-                color: Colors.green,
-                shape: BoxShape.circle,
+            child: Container(
+              alignment: Alignment.center,
+              height: 90,
+              child: ListTile(
+                leading: ClipRRect(
+                  borderRadius: BorderRadius.circular(
+                      THelperFunction.screenHeight() * .3),
+                  child: CachedNetworkImage(
+                    height: 50,
+                    width: 50,
+                    imageUrl: widget.data['image'],
+                    errorWidget: (context, url, error) =>
+                        const CircleAvatar(child: Icon(Iconsax.people)),
+                  ),
+                ),
+                title: Text(widget.data['userName']),
+                trailing: msg == null
+                    ? null
+                    : msg!.read.isEmpty &&
+                            msg!.fromId !=
+                                FirebaseAuth.instance.currentUser!.uid
+                        ? Container(
+                            height: 12,
+                            width: 12,
+                            decoration: const BoxDecoration(
+                              color: Colors.green,
+                              shape: BoxShape.circle,
+                            ),
+                          )
+                        : Text(
+                            THelperFunction.getLastMessageTime(
+                              context: context,
+                              time: msg!.sent,
+                            ),
+                            style: const TextStyle(
+                              color: Colors.black54,
+                            ),
+                          ),
+                subtitle: Text(
+                  msg != null ? msg!.message : widget.data['about'],
+                  maxLines: 1,
+                ),
               ),
             ),
-            subtitle: Text(
-              widget.data['about'],
-              maxLines: 1,
-            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
