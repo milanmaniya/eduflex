a05977b82aa51f9de66d8663ffed628a11e6938a
milanmaniya <milanmaniya2035@gmail.com>
2024-02-18 16:45:46 +0530

    done

--- lib/screen/teacher/dashboard/navigation_menu_screen/teacher_attendance_screen/teacher_attendance_screen.dart
+++ lib/screen/teacher/dashboard/navigation_menu_screen/teacher_attendance_screen/teacher_attendance_screen.dart
@@ -31,12 +31,12 @@ class _TeacherAttendanceScreenState extends State<TeacherAttendanceScreen> {
   final txtSubjectValue = TextEditingController();
 
   List<String> semList = [
-    'SEM1',
-    'SEM2',
-    'SEM3',
-    'SEM4',
-    'SEM5',
-    'SEM6',
+    'FYBCA-SEM1',
+    'FYBCA-SEM2',
+    'SYBCA-SEM3',
+    'SYBCA-SEM4',
+    'TYBCA-SEM5',
+    'TYBCA-SEM6',
   ];
 
   List<String> divisonList = [
@@ -46,117 +46,162 @@ class _TeacherAttendanceScreenState extends State<TeacherAttendanceScreen> {
     'Divison-4',
   ];
 
+  static Stream<QuerySnapshot<Map<String, dynamic>>> getAllClasses() {
+    return FirebaseFirestore.instance.collection('Attendance').snapshots();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-        child: Text('Add Class'),
+      body: StreamBuilder(
+        stream: getAllClasses(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.none ||
+              snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          }
+
+          final data = [];
+
+          if (snapshot.hasData) {
+            for (var element in snapshot.data!.docs) {
+              log(element.id.toString());
+            }
+          }
+
+          if (data.isNotEmpty) {
+            return ListView.separated(
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(data[index]['className']),
+                  subtitle: Row(
+                    children: [
+                      Text(data[index]['sem']),
+                      Text("DIV- $data[index]['divison']")
+                    ],
+                  ),
+                );
+              },
+              separatorBuilder: (context, index) => const SizedBox(
+                height: 10,
+              ),
+              itemCount: data.length,
+            );
+          } else {
+            return const Center(child: Text('Clsss Not Found'));
+          }
+        },
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Iconsax.add),
         onPressed: () {
-          showDialog(
-            barrierDismissible: true,
-            context: context,
-            builder: (context) => AlertDialog(
-              title: const Text('Add Class'),
-              actions: [
-                SizedBox(
-                  width: double.infinity,
-                  child: DropdownButtonFormField2(
-                    decoration: InputDecoration(
-                      contentPadding: const EdgeInsets.symmetric(vertical: 16),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                    ),
-                    onChanged: (value) => semValue = value!,
-                    isExpanded: true,
-                    hint: Text(
-                      semValue.isEmpty ? 'Select Semester' : semValue,
-                    ),
-                    items: semList
-                        .map(
-                          (e) => DropdownMenuItem(
-                            value: e,
-                            child: Text(e),
-                          ),
-                        )
-                        .toList(),
-                  ),
-                ),
-                const SizedBox(
-                  height: 14,
-                ),
-                SizedBox(
-                  width: double.infinity,
-                  child: DropdownButtonFormField2(
-                    decoration: InputDecoration(
-                      contentPadding: const EdgeInsets.symmetric(vertical: 16),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                    ),
-                    onChanged: (value) => divisonValue = value!,
-                    isExpanded: true,
-                    hint: Text(
-                      divisonValue.isEmpty ? 'Select Divison' : divisonValue,
-                    ),
-                    items: divisonList
-                        .map(
-                          (e) => DropdownMenuItem(
-                            value: e,
-                            child: Text(e),
-                          ),
-                        )
-                        .toList(),
-                  ),
-                ),
-                const SizedBox(
-                  height: 14,
-                ),
-                TextFormField(
-                  controller: txtSubjectValue,
-                  validator: ValidationBuilder().required().build(),
-                  decoration: const InputDecoration(
-                    labelText: 'Subject ',
-                    prefixIcon: Icon(Iconsax.info_circle),
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    TextButton(
-                      onPressed: () {
-                        semValue = '';
-                        txtSubjectValue.clear();
-                        Navigator.pop(context);
-                      },
-                      child: const Text('Cancel'),
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        FirebaseFirestore.instance
-                            .collection('data')
-                            .doc('Attendance')
-                            .collection(semValue)
-                            .doc(divisonValue)
-                            .collection(txtSubjectValue.text)
-                            .doc('Student')
-                            .set({}).then((value) {
-                          semValue = '';
-                          txtSubjectValue.clear();
-                        });
-                        Navigator.pop(context);
-                      },
-                      child: const Text('Add'),
-                    ),
-                  ],
-                )
-              ],
-            ),
-          );
+          ShowDialog(context);
         },
+      ),
+    );
+  }
+
+  Future<dynamic> ShowDialog(BuildContext context) {
+    return showDialog(
+      barrierDismissible: true,
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Add Class'),
+        actions: [
+          SizedBox(
+            width: double.infinity,
+            child: DropdownButtonFormField2(
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.symmetric(vertical: 16),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+              onChanged: (value) => semValue = value!,
+              isExpanded: true,
+              hint: Text(
+                semValue.isEmpty ? 'Select Semester' : semValue,
+              ),
+              items: semList
+                  .map(
+                    (e) => DropdownMenuItem(
+                      value: e,
+                      child: Text(e),
+                    ),
+                  )
+                  .toList(),
+            ),
+          ),
+          const SizedBox(
+            height: 14,
+          ),
+          SizedBox(
+            width: double.infinity,
+            child: DropdownButtonFormField2(
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.symmetric(vertical: 16),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+              onChanged: (value) => divisonValue = value!,
+              isExpanded: true,
+              hint: Text(
+                divisonValue.isEmpty ? 'Select Divison' : divisonValue,
+              ),
+              items: divisonList
+                  .map(
+                    (e) => DropdownMenuItem(
+                      value: e,
+                      child: Text(e),
+                    ),
+                  )
+                  .toList(),
+            ),
+          ),
+          const SizedBox(
+            height: 14,
+          ),
+          TextFormField(
+            controller: txtSubjectValue,
+            validator: ValidationBuilder().required().build(),
+            decoration: const InputDecoration(
+              labelText: 'Subject ',
+              prefixIcon: Icon(Iconsax.info_circle),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextButton(
+                onPressed: () {
+                  semValue = '';
+                  txtSubjectValue.clear();
+                  Navigator.pop(context);
+                },
+                child: const Text('Cancel'),
+              ),
+              TextButton(
+                onPressed: () {
+                  FirebaseFirestore.instance
+                      .collection('data')
+                      .doc(semValue)
+                      .set({
+                    'className': txtSubjectValue.text,
+                    'divison': divisonValue,
+                    'sem': semValue
+                  }).then((value) {
+                    txtSubjectValue.clear();
+                    semValue = '';
+                  });
+
+                  Navigator.pop(context);
+                },
+                child: const Text('Add'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
