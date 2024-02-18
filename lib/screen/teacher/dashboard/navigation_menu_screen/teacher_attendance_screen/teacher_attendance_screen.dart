3e27b9f2fa8c1aee357d1c74aea9dd3fcba1dea3
milanmaniya <milanmaniya2035@gmail.com>
2024-02-18 16:16:44 +0530

    done

--- lib/screen/teacher/dashboard/navigation_menu_screen/teacher_attendance_screen/teacher_attendance_screen.dart
+++ lib/screen/teacher/dashboard/navigation_menu_screen/teacher_attendance_screen/teacher_attendance_screen.dart
@@ -1,6 +1,9 @@
 import 'dart:developer';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dropdown_button2/dropdown_button2.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:form_validator/form_validator.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:iconsax/iconsax.dart';
 
@@ -24,9 +27,9 @@ class _TeacherAttendanceScreenState extends State<TeacherAttendanceScreen> {
     super.initState();
   }
 
-  bool isBca = false;
-
   String semValue = '';
+  String divisonValue = '';
+  final txtSubjectValue = TextEditingController();
 
   List<String> semList = [
     'SEM1',
@@ -35,6 +38,13 @@ class _TeacherAttendanceScreenState extends State<TeacherAttendanceScreen> {
     'SEM4',
     'SEM5',
     'SEM6',
+  ];
+
+  List<String> divisonList = [
+    'Divison-1',
+    'Divison-2',
+    'Divison-3',
+    'Divison-4',
   ];
 
   @override
@@ -47,44 +57,99 @@ class _TeacherAttendanceScreenState extends State<TeacherAttendanceScreen> {
         child: const Icon(Iconsax.add),
         onPressed: () {
           showDialog(
+            barrierDismissible: true,
             context: context,
             builder: (context) => AlertDialog(
               title: const Text('Add Class'),
               actions: [
-                // SizedBox(
-                //   width: double.infinity,
-                //   child: DropdownButtonFormField2(
-                //     decoration: InputDecoration(
-                //       contentPadding: const EdgeInsets.symmetric(vertical: 16),
-                //       border: OutlineInputBorder(
-                //         borderRadius: BorderRadius.circular(15),
-                //       ),
-                //     ),
-                //     onChanged: (value) => semValue = value!,
-                //     isExpanded: true,
-                //     hint: Text(
-                //       semValue.isEmpty ? 'Select Your Year' : semValue,
-                //     ),
-                //     items: semList
-                //         .map(
-                //           (e) => DropdownMenuItem(
-                //             value: e,
-                //             child: Text(e),
-                //           ),
-                //         )
-                //         .toList(),
-                //   ),
-                // ),
-
+                SizedBox(
+                  width: double.infinity,
+                  child: DropdownButtonFormField2(
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(vertical: 16),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                    onChanged: (value) => semValue = value!,
+                    isExpanded: true,
+                    hint: Text(
+                      semValue.isEmpty ? 'Select Semester' : semValue,
+                    ),
+                    items: semList
+                        .map(
+                          (e) => DropdownMenuItem(
+                            value: e,
+                            child: Text(e),
+                          ),
+                        )
+                        .toList(),
+                  ),
+                ),
+                const SizedBox(
+                  height: 14,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: DropdownButtonFormField2(
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(vertical: 16),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                    onChanged: (value) => divisonValue = value!,
+                    isExpanded: true,
+                    hint: Text(
+                      divisonValue.isEmpty ? 'Select Divison' : divisonValue,
+                    ),
+                    items: divisonList
+                        .map(
+                          (e) => DropdownMenuItem(
+                            value: e,
+                            child: Text(e),
+                          ),
+                        )
+                        .toList(),
+                  ),
+                ),
+                const SizedBox(
+                  height: 14,
+                ),
+                TextFormField(
+                  controller: txtSubjectValue,
+                  validator: ValidationBuilder().required().build(),
+                  decoration: const InputDecoration(
+                    labelText: 'Subject ',
+                    prefixIcon: Icon(Iconsax.info_circle),
+                  ),
+                ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
                     TextButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        semValue = '';
+                        txtSubjectValue.clear();
+                        Navigator.pop(context);
+                      },
                       child: const Text('Cancel'),
                     ),
                     TextButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        FirebaseFirestore.instance
+                            .collection('Teacher')
+                            .doc(FirebaseAuth.instance.currentUser!.uid)
+                            .collection('Attendance')
+                            .doc(semValue)
+                            .collection(divisonValue)
+                            .doc(txtSubjectValue.text)
+                            .set({}).then((value) {
+                          semValue = '';
+                          txtSubjectValue.clear();
+                        });
+                        Navigator.pop(context);
+                      },
                       child: const Text('Add'),
                     ),
                   ],
