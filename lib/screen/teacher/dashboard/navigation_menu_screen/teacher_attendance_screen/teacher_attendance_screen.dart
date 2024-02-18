b0715e8a38c1dba0c2b9407f7022dcc9d5e3727e
milanmaniya <milanmaniya2035@gmail.com>
2024-02-18 14:49:07 +0530

    done

--- lib/screen/teacher/dashboard/navigation_menu_screen/teacher_attendance_screen/teacher_attendance_screen.dart
+++ lib/screen/teacher/dashboard/navigation_menu_screen/teacher_attendance_screen/teacher_attendance_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get_storage/get_storage.dart';
 import 'package:iconsax/iconsax.dart';
 
 class TeacherAttendanceScreen extends StatefulWidget {
@@ -11,8 +10,6 @@ class TeacherAttendanceScreen extends StatefulWidget {
 }
 
 class _TeacherAttendanceScreenState extends State<TeacherAttendanceScreen> {
-  final localStorage = GetStorage();
-
   bool isBca = false;
 
   String semValue = '';
