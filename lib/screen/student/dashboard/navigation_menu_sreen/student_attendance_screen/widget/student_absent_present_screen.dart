ce2163140a9f6fd73545300e2751f445856f5cec
milanmaniya <milanmaniya2035@gmail.com>
2024-02-24 15:56:24 +0530

    done

--- lib/screen/student/dashboard/navigation_menu_sreen/student_attendance_screen/widget/student_absent_present_screen.dart
+++ lib/screen/student/dashboard/navigation_menu_sreen/student_attendance_screen/widget/student_absent_present_screen.dart
@@ -1,7 +1,5 @@
 import 'dart:developer';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:eduflex/utils/constant/sizes.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
