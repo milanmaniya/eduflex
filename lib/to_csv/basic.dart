2cf41a739c9de5e5c38daf4b6a2594c3a4b6d697
milanmaniya <milanmaniya2035@gmail.com>
2024-02-25 16:55:54 +0530

    done

--- lib/to_csv/basic.dart
+++ lib/to_csv/basic.dart
@@ -8,13 +8,23 @@ class BasicScreen extends StatefulWidget {
 }
 
 class _BasicScreenState extends State<BasicScreen> {
+  Future<void> createExcel() async {
+    
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {},
-          child: const Text('Export Csv Data'),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {},
+              child: const Text('Export Csv Data'),
+            ),
+          ),
         ),
       ),
     );
