df5c6cd4990df9e0fd071d7fd943902eec22a0dd
milanmaniya <milanmaniya2035@gmail.com>
2024-02-25 17:11:36 +0530

    done

--- lib/to_csv/basic.dart
+++ lib/to_csv/basic.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+import 'package:open_filex/open_filex.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_xlsio/xlsio.dart';
 
 class BasicScreen extends StatefulWidget {
   const BasicScreen({super.key});
@@ -9,7 +13,23 @@ class BasicScreen extends StatefulWidget {
 
 class _BasicScreenState extends State<BasicScreen> {
   Future<void> createExcel() async {
-    
+    final Workbook workbook = Workbook();
+
+    final Worksheet sheet = workbook.worksheets[0];
+
+    sheet.getRangeByName('A1').setText('Milan');
+    final List<int> bytes = workbook.saveAsStream();
+    workbook.dispose();
+
+    final String path = (await getApplicationSupportDirectory()).path;
+
+    final String fileName = '$path/Example.xlsx';
+
+    final File file = File(fileName);
+
+    await file.writeAsBytes(bytes);
+
+    OpenFilex.open(fileName);
   }
 
   @override
@@ -21,7 +41,7 @@ class _BasicScreenState extends State<BasicScreen> {
           child: SizedBox(
             width: double.infinity,
             child: ElevatedButton(
-              onPressed: () {},
+              onPressed: createExcel,
               child: const Text('Export Csv Data'),
             ),
           ),
